51b79a2b22d896bfeff0e1e6698f0832f3c1a56d
mykola9208 <117111441+mykola9208@users.noreply.github.com>
2022-11-28 02:42:59 +0300

    homework_5

--- main.py
+++ main.py
@@ -16,16 +16,41 @@ def grep(pattern):
             desired = None
 
 
+
 def add_word(word):
     global WORDS
-    temporary = WORDS
+    TEMP = WORDS
+    def inner_func(TEMP, inn):
+        for i in range(0, len(inn) + 1):
+            if i == len(inn):
+                TEMP.setdefaul('TERM', word)
+            elif TEMP.get(inn[i]) is None:
+                TEMP.setdefault(inn[i], inner_func({}, inn[i + 1:len(inn)]))
+            else:
+                TEMP.update(inn[i], inner_func(TEMP.get(inn[i]), inn[i + 1:len(inn)]))
+        return TEMP
+    return inner_func
+
+def add_word(word):
+    global WORDS
+    temp = WORDS
+    num = 0
     def inner_func(inn):
         for i in range(0, len(inn) + 1):
             if i == len(inn):
                 return {'TERM': word}
             else:
                 return {inn[i]: inner_func(inn[i + 1:len(inn)])}
-    return inner_func(word)
+    while temp.get(word[num]) is not None and num < len(word):
+        temp = temp.get(word[num])
+        num += 1
+    temp.setdefault(word[num], inner_func(word[num+1:len(word)]))
+    while num > 0:
+        num -= 1
+        temp = {word[num]: temp}
+    WORDS = temp
+    return WORDS
+
 
 
 if __name__ == '__main__':
@@ -44,6 +69,10 @@ if __name__ == '__main__':
     assert search.send('but you better be quick (bbq) otherwise') == 'but you better be quick (bbq) otherwise'
     search.close()
 
-    print(add_word('hello'))
-
+    add_word('hello')
+    print(WORDS)
+    add_word('her')
+    print(WORDS)
+    add_word('help')
+    print(WORDS)
 
